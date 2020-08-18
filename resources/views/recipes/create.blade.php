155023b1b98582f087735c6abf4b17efda690fd6
Staffing07 <57902211+Staffing07@users.noreply.github.com>
2020-08-18 22:56:36 +0100

    Styled create recipe blade
    no longer using add ingredients form.  Figure quicker to just type than be selecting dropdowns and such for an ingredients list

--- resources/views/recipes/create.blade.php
+++ resources/views/recipes/create.blade.php
@@ -2,82 +2,95 @@
 
 @section('content')
 
-<div class="container mx-auto">
-    <div class="w-full">
 
-        <form class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4" action="/recipes" method="POST">
-            <h1 class="text-2xl">Add a recipe</h1>
-            <div class="mb-4">
-                <!-- csrf required to allow cross site forgery protection else form will not work -->
-                @csrf
-                <label class="block text-gray-700 text-sm font-bold mb-2" for="username" for="name">Recipe name</label>
-                <input
-                    class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                    type="text" id="name" name="name">
-            </div>
 
-            <div class="mb-4">
-                <label class="block text-gray-700 text-sm font-bold mb-2" for="course">Course</label>
-                <select
-                    class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                    id="course" name="course">
-                    <option value="starter">Starter</option>
-                    <option value="main">Main</option>
-                    <option value="dessert">Dessert</option>
-                    <option value="side">Side</option>
-                </select>
-            </div>
 
-            <div class="mb-4">
-                <label class="block text-gray-700 text-sm font-bold mb-2" for="cuisine">Cuisine</label>
-                <select
-                    class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                    name="cuisine" id="cuisine">
-                    <option value="british">British</option>
-                    <option value="chinese">Chinese</option>
-                    <option value="american">American</option>
-                    <option value="indian">Indian</option>
-                    <option value="italian">Italian</option>
-                    <option value="greek">Greek</option>
-                </select>
-            </div>
+<form class="w-full mx-auto max-w-xl py-24" action="/recipes" method="POST">
+    <div class="flex flex-wrap -mx-3 mb-6">
 
-            <div class="mb-4">
-                <label class="block text-gray-700 text-sm font-bold mb-2" for="description">Description</label>
-                <input
-                    class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                    type="text" id="description" name="description">
-            </div>
 
-            <div class="mb-4">
-                <label class="block text-gray-700 text-sm font-bold mb-2" for="serves">Serves</label>
-                <input
-                    class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                    type="text" id="serves" name="serves">
-            </div>
+        <div class="w-full px-3">
+            <h1 class="text-2xl font-extrabold">Add a recipe</h1>
+            <!-- csrf required to allow cross site forgery protection else form will not work -->
+            @csrf
+            <label class="block text-gray-700 text-sm font-bold mb-2" for="username" for="name">Recipe name</label>
+            <input
+                class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                type="text" id="name" name="name">
+        </div>
+    </div>
 
-            <div class="mb-4">
-                <label class="block text-gray-700 text-sm font-bold mb-2" for="preptime">Prep time</label>
-                <input
-                    class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                    type="text" id="preptime" name="preptime">
-            </div>
+    <div class="flex flex-wrap -mx-3 mb-6">
+        <div class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+            <label class="block text-gray-700 text-sm font-bold mb-2" for="course">Course</label>
+            <select
+                class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                id="course" name="course">
+                <option value="starter">Starter</option>
+                <option value="main">Main</option>
+                <option value="dessert">Dessert</option>
+                <option value="side">Side</option>
+            </select>
+        </div>
 
-            <div class="mb-4">
-                <label class="block text-gray-700 text-sm font-bold mb-2" for="cookingtime">Cooking time</label>
-                <input
-                    class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                    type="cookingtime" id="cookingtime" name="cookingtime">
-            </div>
 
-            <div class="mb-4">
-                <label class="block text-gray-700 text-sm font-bold mb-2" for="ingredientMeasure">Amount</label>
-                <input
-                    class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                    type="number" name="ingredientMeasure" step="0.25">
-            </div>
-<!-- Pulls from DB -->
-            <!--<div class="mb-4">
+        <div class="w-full md:w-1/2 px-3">
+            <label class="block text-gray-700 text-sm font-bold mb-2" for="cuisine">Cuisine</label>
+            <select
+                class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                name="cuisine" id="cuisine">
+                <option value="british">British</option>
+                <option value="chinese">Chinese</option>
+                <option value="american">American</option>
+                <option value="indian">Indian</option>
+                <option value="italian">Italian</option>
+                <option value="greek">Greek</option>
+            </select>
+        </div>
+    </div>
+
+    <div class="flex flex-wrap -mx-3 mb-6">
+        <div class="w-full px-3">
+            <label class="block text-gray-700 text-sm font-bold mb-2" for="description">Description</label>
+            <textarea
+                class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                id="description" name="description" rows="10" cols="70"></textarea>
+        </div>
+    </div>
+
+    <div class="flex flex-wrap -mx-3 mb-2">
+        <div class="w-full md:w-1/3 px-3 mb-6 md:mb-0">
+            <label class="block text-gray-700 text-sm font-bold mb-2" for="serves">Serves</label>
+            <input
+                class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                type="text" id="serves" name="serves">
+        </div>
+
+        <div class="w-full md:w-1/3 px-3 mb-6 md:mb-0">
+            <label class="block text-gray-700 text-sm font-bold mb-2" for="preptime">Prep time</label>
+            <input
+                class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                type="text" id="preptime" name="preptime">
+        </div>
+
+        <div class="w-full md:w-1/3 px-3 mb-6 md:mb-0">
+            <label class="block text-gray-700 text-sm font-bold mb-2" for="cookingtime">Cooking time</label>
+            <input
+                class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                type="cookingtime" id="cookingtime" name="cookingtime">
+        </div>
+    </div>
+
+    <div class="flex flex-wrap -mx-3 mb-6">
+        <div class="w-full px-3">
+            <label class="block text-gray-700 text-sm font-bold mb-2" for="description">Ingredients</label>
+            <textarea
+                class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                id="ingredients" name="ingredients" rows="10" cols="70"></textarea>
+        </div>
+    </div>
+    <!-- Pulls from DB -->
+    <!--<div class="mb-4">
                 <label class="block text-gray-700 text-sm font-bold mb-2" for="ingredientUnit">Unit</label>
                 
                 
@@ -92,41 +105,46 @@
                     <option value="tbsp">Tablespoon(s)</option>
                 </select>
             </div>-->
+    <!--
+    <div class="mb-4">
+        <select
+            class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            name="measurements[]" id="measurements">
+            @foreach ($measurements as $measurement)
+            <option value="1">{{ $measurement }}</option>
 
-            <div class="mb-4">
-                <select
-                    class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                    name="measurements[]" id="measurements">
-                    @foreach ($measurements as $measurement)
-                    <option value="1">{{ $measurement }}</option>
-
-                    @endforeach
-                </select>
-            </div>
-
-            <div class="mb-4">
-                <fieldset>
-                    <label class="block text-gray-700 text-sm font-bold mb-2">Ingredients</label>
-                    <input type="checkbox" name="ingredients[]" value="mushrooms">Mushrooms<br />
-                    <input type="checkbox" name="ingredients[]" value="cheese">Cheese<br />
-                    <input type="checkbox" name="ingredients[]" value="garlic">Olives<br />
-                </fieldset>
-            </div>
-
-            <div class="mb-4">
-                <label class="block text-gray-700 text-sm font-bold mb-2" for="method">Method</label>
-                <input
-                    class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                    type="text" name="method" id="method">
-            </div>
-
-            <div class="mb-4">
-                <input type="submit" value="Add recipe" name="addrecipe">
-            </div>
-        </form>
-
+            @endforeach
+        </select>
     </div>
-</div>
+
+    <div class="mb-4">
+        <fieldset>
+            <label class="block text-gray-700 text-sm font-bold mb-2">Ingredients</label>
+            <input type="checkbox" name="ingredients[]" value="mushrooms">Mushrooms<br />
+            <input type="checkbox" name="ingredients[]" value="cheese">Cheese<br />
+            <input type="checkbox" name="ingredients[]" value="garlic">Olives<br />
+        </fieldset>
+    </div>-->
+
+    <div class="flex flex-wrap -mx-3 mb-6">
+        <div class="w-full px-3">
+            <label class="block text-gray-700 text-sm font-bold mb-2" for="description">Method</label>
+            <textarea
+                class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                id="method" name="method" rows="10" cols="70"></textarea>
+        </div>
+    </div>
+
+    <div class="flex flex-wrap -mx-3 mb-6">
+        <div class="w-full px-3">
+            <input class="bg-blue-500 cursor-pointer hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" type="submit"
+                value="Add recipe" name="addrecipe">
+        </div>
+    </div>
+</form>
+
+
+
 @endsection
 
 
--- resources/views/recipes/show.blade.php
+++ resources/views/recipes/show.blade.php
@@ -3,15 +3,16 @@
 @section('content')
 
 <div class="wrapper pizza-details">
+    <p>{{ $recipe->course }}</p>
     <h1>Recipe {{ $recipe->name }}</h1>
+    <p>{{ $recipe->cuisine }}</p>
     <p class="lead">Description - {{ $recipe -> description}}</p>
     <p>Serves {{ $recipe->serves }}</p>
-    <p class="ingredients">Ingredients</p>
-    <ul>
-        @foreach ($recipe->ingredients as $ingredient)
-        <li>{{ $ingredient }}</li>
-        @endforeach
-    </ul>
+    <p>Serves {{ $recipe->preptime }}</p>
+    <p>Serves {{ $recipe->cookingtime }}</p>
+    
+    <p>Ingredients {{ $recipe->ingredients }}</p>
+    <p>Method {{ $recipe->method }}</p>
 </div>
 <a href="/recipes" class="back">Back to recipes</a>
 @endsection
